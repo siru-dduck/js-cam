feb1f0cacda50cd7e01a99ecdbcead6693ac41a3
shinwoo <sinwoo1225@gmail.com>
2020-03-29 00:36:34 +0900

    first commit

---
+++ js/app.js
@@ -0,0 +1,64 @@
+var video = document.querySelector("#videoElement");
+var video2 = document.querySelector("#videoElement2");
+var startBtn = document.querySelector("#start");
+var stopBtn = document.querySelector("#stop");
+var btnContainer = document.querySelector(".btn-container");
+var mediaRecorder = null;
+var chunks = [];
+var stream;
+
+function handleDataAvailable(e) {
+  console.log("push chunks");
+  chunks.push(e.data);
+  var blob = new Blob(chunks, { type: "video/x-matroska;codecs=avc1" });
+  chunks = [];
+  video2.src = URL.createObjectURL(blob);
+  saveVideo(blob);
+}
+
+function saveVideo(data) {
+  const link = document.createElement("a");
+  const btn = document.createElement("button");
+  link.href = URL.createObjectURL(data);
+  link.download = "Video.mkv";
+  btn.innerText = "save";
+  link.appendChild(btn);
+  btnContainer.appendChild(link);
+}
+
+function init() {
+  if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices
+      .getUserMedia({ video: { width: 1280, height: 720 }, audio: true })
+      .then(function(stream) {
+        video.srcObject = stream;
+        video.muted = true;
+        mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.addEventListener("dataavailable", handleDataAvailable);
+      })
+      .catch(function(error) {
+        console.log("Something went wrong!");
+        console.dir(error);
+      });
+  }
+
+  startBtn.addEventListener("click", function(e) {
+    if (mediaRecorder) {
+      mediaRecorder.start();
+      console.log(mediaRecorder.state);
+    } else {
+      console.log("MediaRecorder not exist");
+    }
+  });
+
+  stopBtn.addEventListener("click", function(e) {
+    if (mediaRecorder) {
+      mediaRecorder.stop();
+      console.log(mediaRecorder.state);
+    } else {
+      console.log("MediaRecorder not exist");
+    }
+  });
+}
+
+init();
