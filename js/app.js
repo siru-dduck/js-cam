f744c17943752adb20e6ed9dfce40898aab3e81c
shinwoo <sinwoo1225@gmail.com>
2022-03-05 22:57:15 +0900

    여러개의 동영상을 이어붙이는 예제로 수정

--- js/app.js
+++ js/app.js
@@ -9,25 +9,37 @@ var mediaSource;
 var sourceBuffer;
 var buffer;
 
+var i = 0;
 async function handleDataAvailable(e) {
-  console.log("push chunks");
-  chunks.push(e.data);
+  try {
+    console.log("push chunks");
+    chunks.push(e.data);
 
-  var blob = new Blob(chunks, { type: "video/x-matroska;codecs=avc1" });
-  chunks = [];
+    var blob = new Blob(chunks, { type: 'video/webm;codecs=vp8,opus' });
+    chunks = [];
 
-  buffer = await blob.arrayBuffer();
-  mediaSource = new MediaSource();
-  video2.src = URL.createObjectURL(mediaSource);
-  mediaSource.addEventListener("sourceopen", function() {
-    sourceBuffer = mediaSource.addSourceBuffer();
-    sourceBuffer.addEventListener("updateend", function() {
-      mediaSource.endOfStream();
-      video2.play();
+    buffer = await blob.arrayBuffer();
+    mediaSource = new MediaSource();
+    video2.src = URL.createObjectURL(mediaSource);
+    mediaSource.addEventListener("sourceopen", async function () {
+      sourceBuffer = mediaSource.addSourceBuffer('video/webm;codecs=vp8,opus');
+      sourceBuffer.mode = "sequence";
+      sourceBuffer.addEventListener("updateend", function() {
+        console.log("update end");
+        if(i++ < 3) {
+          sourceBuffer.appendBuffer(buffer);
+          return;
+        }
+        mediaSource.endOfStream(); // 동영상 스트림이 끝났음을 알림
+        video2.play();
+      });
+      sourceBuffer.appendBuffer(buffer);
     });
-    sourceBuffer.appendBuffer(buffer);
-  });
-  saveVideo(blob);
+    saveVideo(blob);
+  } catch(err) {
+    console.log("삐삐");
+    console.error(err);
+  }
 }
 
 function saveVideo(data) {
@@ -44,19 +56,22 @@ function init() {
   if (navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices
       .getUserMedia({ video: { width: 1280, height: 720 }, audio: true })
-      .then(function(stream) {
+      .then(function (stream) {
         video.srcObject = stream;
         video.muted = true;
-        mediaRecorder = new MediaRecorder(stream);
+        const options = {
+          mimeType: 'video/webm;codecs=vp8'
+        }
+        mediaRecorder = new MediaRecorder(stream, options);
         mediaRecorder.addEventListener("dataavailable", handleDataAvailable);
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log("Something went wrong!");
         console.dir(error);
       });
   }
 
-  startBtn.addEventListener("click", function(e) {
+  startBtn.addEventListener("click", function (e) {
     if (mediaRecorder) {
       mediaRecorder.start();
       console.log(mediaRecorder.state);
@@ -65,7 +80,7 @@ function init() {
     }
   });
 
-  stopBtn.addEventListener("click", function(e) {
+  stopBtn.addEventListener("click", function (e) {
     if (mediaRecorder) {
       mediaRecorder.stop();
       console.log(mediaRecorder.state);
